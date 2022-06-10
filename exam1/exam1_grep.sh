1199c18912573156b99204abe12b9d3da52a19e4
DanielaChavez <daniela.chavez@est.ikiam.edu.ec>
2022-06-10 16:27:49 -0500

    Resolución de actividades Examen Parcial 1

--- exam1/exam1_grep.sh
+++ exam1/exam1_grep.sh
@@ -1,12 +1,17 @@
 #! /bin/bash
 #Actividad 1
 	$inter = ../data/interleukin.txt
+	$inter
 #Actividad 2
 	mkdir grep
+	touch grep/cattle.txt grep/chicken.txt grep/linear_dna.txt
 #Actividad 3.1
 	grep "cattle" $inter > ../grep/cattle.txt
+	cat cattle.txt >> ../grep/cattle.txt
 #Actividad 3.2
 	grep "chicken" $inter > ../grep/chicken.txt
+	cat chicken.txt
 #Actividad 4
 	grep "linear DNA" $inter > ../grep/linear_dna.txt
 	sort -n linear_dna.txt
+	cat linear_dna.txt
